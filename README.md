5830c971dac1118ae42eb7e4dfe0203049363fbc
Cristian Romanescu <cristian@romanescu.ro>
2018-10-09 16:00:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Install
+
+Install node (>= 8.10.0) and npm (>= 3.5.2).
+
+```
+git checkout project
+npm i
+```
+
+Copy `example.config.yml` to `config.yml` and customize if necessary.
+
+# Usage
+
+## Taking comparison screenshots
+
+```
+DEBUG_LEVEL=DEBUG node index.js compare
+```
+
+## Taking historical screenshots
+
+```
+DEBUG_LEVEL=DEBUG node index.js history
+```
---
+++ index.js
@@ -0,0 +1,132 @@
+const readYaml = require('read-yaml');
+const puppeteer = require('puppeteer');
+const Log = require('debug-level');
+const sprintfjs = require('sprintf-js');
+const sprintf = sprintfjs.sprintf;
+
+(async () => {
+  const log = new Log('main')
+  var command = null;
+  if (process.argv[2]) {
+    command = process.argv[2];
+  }
+  else {
+    log.error("Usage: node index.js <compare|history>");
+    return -1;
+  }
+  var config = readYaml.sync('config.yml');
+  log.info('Executing command: ' + command);
+
+  if (command == 'compare') {
+    var items = new Map();
+    Object.keys(config.compare.links).forEach(function(key) {
+      var path = config.compare.links[key];
+      var prod_site = config.compare.url.prod;
+      var test_site = config.compare.url.test;
+      items.set(key, {
+        "prod_url": prod_site + path,
+        "test_url": test_site + path,
+        "prod_image": config.compare.output + '/' + key + '_prod.png',
+        "test_image": config.compare.output + '/' + key + '_test.png',
+        "diff_image": config.compare.output + '/' + key + '_diff.png',
+      });
+    });
+
+    for (var item of items.entries()) {
+      var key = item[0], ob = item[1];
+      var element;
+
+      var screen_width = 1280;
+      var screen_height = 15000;
+
+      const browser = await puppeteer.launch();
+      var page = await browser.newPage();
+      page.setViewport({width: screen_width, height: screen_height});
+      log.debug('%s: Getting PROD screenshot %s to %s ', key, ob.prod_url, ob.prod_image);
+
+      await page.goto(ob.prod_url);
+
+      /******************************************************************/
+      // Do some clicking to expand all page before screenshot
+      if (element = await page.$('a[href="#full-assessment"]')) {
+        await element.click();
+        await page.waitFor(500);
+      }
+      if (element = await page.$('.expand-button')) {
+        await element.click();
+        await page.waitFor(1000);
+      }
+      /******************************************************************/
+
+      await page.screenshot({path: ob.prod_image});
+
+      var page = await browser.newPage();
+      page.setViewport({width: screen_width, height: screen_height});
+      log.debug('%s: Getting TEST screenshot %s to %s', key, ob.prod_url, ob.test_image);
+
+      await page.goto(ob.test_url);
+
+      /******************************************************************/
+      // Do some clicking to expand all page before screenshot
+      if (element = await page.$('a[href="#full-assessment"]')) {
+        await element.click();
+        await page.waitFor(500);
+      }
+      if (element = await page.$('.expand-button')) {
+        await element.click();
+        await page.waitFor(1000);
+      }
+      /******************************************************************/
+      await page.screenshot({path: ob.test_image});
+      await browser.close();
+
+      const { spawnSync} = require('child_process');
+      log.debug('%s: Creating DIFF image %s', key, ob.diff_image);
+      const child_rm = spawnSync('rm', [ob.diff_image]);
+      log.debug(config.compare.compare_bin + ' ' + ob.prod_image + ' ' + ob.test_image + ' ' + '-highlight-color red ' +  ob.diff_image);
+      const child_compare = spawnSync(config.compare.compare_bin, [ob.prod_image, ob.test_image, '-highlight-color', 'red', ob.diff_image]);
+      log.info('Finished: %s', key);
+    }
+  }
+
+  if (command == 'history') {
+    var items = new Map();
+    Object.keys(config.history.links).forEach(function(key) {
+      var path = config.history.links[key];
+      var prod_site = config.history.url;
+      items.set(key, {
+        "prod_url": prod_site + path,
+        "prod_image": config.history.output + '/' + key + '.png',
+      });
+    });
+
+    for (var item of items.entries()) {
+      var key = item[0], ob = item[1];
+      var element;
+      var screen_width = 1280;
+      var screen_height = 15000;
+
+      const browser = await puppeteer.launch();
+      var page = await browser.newPage();
+      page.setViewport({width: screen_width, height: screen_height});
+      log.debug('%s: Getting PROD screenshot %s to %s ', key, ob.prod_url, ob.prod_image);
+      await page.goto(ob.prod_url);
+
+      var element;
+      /******************************************************************/
+      // Do some clicking to expand all page before screenshot
+      if (element = await page.$('a[href="#full-assessment"]')) {
+        await element.click();
+        await page.waitFor(500);
+        element = await page.$('.expand-button');
+        await element.click();
+        await page.waitFor(1000);
+      }
+      /******************************************************************/
+
+      await page.screenshot({path: ob.prod_image});
+      await browser.close();
+    }
+    log.info('Done');
+  }
+})();
